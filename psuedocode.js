7e9fb65ef131b4f193f74fdfb20fbf7d4ae78c74
kimnana45 <kimle.lek@gmail.com>
2020-08-04 18:30:36 -0400

    psuedo code

---
+++ psuedocode.js
@@ -0,0 +1,25 @@
+//Components
+    //App
+        //state, what you type in (lives in app)
+        //Header
+            //no state
+        //Searchbar
+            //props, what you type in 
+        //Table 
+            //state, how data is sorted, and the list of employees
+            //props, what you type in
+            //Columns
+                //clicking on column to change how data is sorted 
+            //Functionality of table 
+                //map over employees, make an employee row for each one
+                //depending on the column clicked, sort data appropriately  
+                    //sort the array 
+                    //set the state of the table 
+                //Searchbar text 
+                    //filter the employees by some sort of citeria
+                    //does a string contain characters? How to do it/
+                    //Make sure to maintain the original list 
+            //Employee Row
+                //show a single employee
+//for getting the list of employees
+//https://randomuser.me/api/?results=200&nat=us
